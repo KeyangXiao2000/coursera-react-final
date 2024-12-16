a0616eb6a812e66b1efe00d1ac4b67c1723cde4a
KeyangXiao2000 <keyang.xiao.2000@gmail.com>
2024-12-15 21:12:31 -0500

    implemented meals, took a while to fix bugs. Working on cart

--- src/store.js
+++ src/store.js
@@ -2,12 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice';
-//import mealsReducer from './mealsSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
     av: avReducer,
-    //meals: mealsReducer,
+    meals: mealsReducer,
   },
 });
